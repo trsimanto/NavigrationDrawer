d09354745eca6a49575243970e0dc4e4c8412f90
Towhid <towhidurrahmansimanto@gmail.com>
2018-08-21 16:58:45 +0600

    drawer with fragment

--- app/src/main/java/com/example/towhid/navigrationdrawer/MainActivity.java
+++ app/src/main/java/com/example/towhid/navigrationdrawer/MainActivity.java
@@ -1,20 +1,69 @@
 package com.example.towhid.navigrationdrawer;
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar=findViewById(R.id.toolbar);
+        Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigration_drawer_open, R.string.navigration_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
 
 
+        if (savedInstanceState == null) { //if use korse jate rotation korle change na hoy , ata na dele change hoea jay
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container
+                    , new MassageFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_massage);
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START))
+            drawer.closeDrawer(GravityCompat.START);
+        else
+            super.onBackPressed();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_massage:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container
+                        , new MassageFragment()).commit();
+                break;
+            case R.id.nav_chat:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container
+                        , new ChatFragment()).commit();
+                break;
+            case R.id.nav_profile:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container
+                        , new ProfileFragment()).commit();
+                break;
+        }
+        drawer.closeDrawer(GravityCompat.START);
+
+        return true;
     }
 }
